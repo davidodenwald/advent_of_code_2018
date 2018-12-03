6dea8b9aafd0084eb054490613abde01c7379a01
davidodenwald <david.odenwald91@gmail.com>
2018-12-03 14:32:12 +0100

    added day1 challenge 2

--- day1/chronal_calibration.go
+++ day1/chronal_calibration.go
@@ -16,14 +16,41 @@ func main() {
 		log.Fatalln("file not found")
 	}
 
-	sum := 0
+	var nums []int
 	res := strings.Split(string(file), "\n")
 	for x, line := range res {
 		num, err := strconv.Atoi(line)
 		if err != nil {
-			log.Fatalf("%s:%d '%s' is not a number", filename, x+1, line)
+			log.Printf("%s:%d '%s' is not a number", filename, x+1, line)
 		}
+		nums = append(nums, num)
+	}
+
+	part1(nums)
+	part2(nums)
+}
+
+func part1(nums []int) {
+	var sum int
+	for _, num := range nums {
 		sum += num
 	}
 	fmt.Println(sum)
+}
+
+func part2(nums []int) {
+	var sum int
+	hist := make(map[int]bool)
+
+	var i int
+	for true {
+		_, double := hist[sum]
+		if double {
+			fmt.Println(sum)
+			return
+		}
+		hist[sum] = true
+		sum += nums[i]
+		i = (i + 1) % len(nums)
+	}
 }
